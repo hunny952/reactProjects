b737b6cc4237102969b2a36787ea5c2d2cacaece
hunny952 <engineerhunny@gmail.com>
2024-07-01 18:02:46 +0500

    change header styling and fixed sorting issue

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TableCell, TableRow, Box } from "@mui/material";
 import styled from "@emotion/styled";
+import "../Components.css";
 
 const Styledtableheadercell = styled(TableCell)({
       fontFamily: "system-ui",
@@ -13,58 +14,58 @@ const Styledtableheadercell = styled(TableCell)({
 
 const TableHeader = ({ handleSort, getSortIcon }) => {
   const headerCells = [
-    "Hair Color",
-    "Skin Color",
-    "Gender",
-    "Eye Color",
-    "Birth Year",
+    { key: "hair_color", label: "Hair Color" },
+    { key: "skin_color", label: "Skin Color" },
+    { key: "gender", label: "Gender" },
+    { key: "eye_color", label: "Eye Color" },
+    { key: "birth_year", label: "Birth Year" },
   ];
 
   const getHeaderCellClassName = (key) => {
     switch (key) {
       case "Hair Color":
-        return "relative tracking-[0.02em] font-medium inline-block min-w-[51.2px] z-[1]";
+        return "header-cell-hair-color";
       case "Skin Color":
-        return "relative tracking-[0.02em] font-medium z-[1]";
+        return "header-cell-skin-color";
       case "Eye Color":
-        return "relative tracking-[0.02em] font-medium inline-block min-w-[129.5px] z-[1]";
+        return "header-cell-eye-color";
       case "Birth Year":
-        return "relative tracking-[0.02em] font-medium inline-block min-w-[56.9px] z-[1]";
+        return "header-cell-birth-year";
       case "Gender":
-        return "relative tracking-[0.02em] font-medium inline-block min-w-[96.8px] z-[1]";
+        return "header-cell-gender";
       default:
-        return "w-32 bg-white";
+        return "header-cell-default";
     }
   };
 
   return (
     <TableRow>
       <Styledtableheadercell>
-        <Box className="flex flex-row items-start justify-start gap-[14.2px]">
+        <Box className="header-cell-box">
           <input
-            className="m-0 h-[25.6px] w-[25.6px] relative top-[11px] [filter:drop-shadow(0px_2.8px_5.69px_rgba(0,_28,_63,_0.21))] rounded-[7.11px] box-border z-[1] border-[1.4px] border-solid border-gainsboro"
+            className="header-cell-checkbox"
             type="checkbox"
           />
-          <Box className="flex flex-col items-start justify-start pt-[2.9px] px-0 pb-0">
+          <Box className="header-cell-column">
             <Box
               onClick={() => handleSort("name")}
-              className="relative tracking-[0.02em] font-medium inline-block min-w-[81.1px] z-[1]"
+              className="name-header-styling"
             >
               Name {getSortIcon("name")}
             </Box>
           </Box>
         </Box>
       </Styledtableheadercell>
-      {headerCells.map((key) => (
+
+      {headerCells.map(({ key, label }) => (
         <Styledtableheadercell key={key}>
-          <Box
-            onClick={() => handleSort(key)}
-            className={`${getHeaderCellClassName(
-              key
-            )}`}
+        <Box
+          onClick={() => handleSort(key)}
+          className={`${getHeaderCellClassName(key)}`}
+          style={{ cursor: "pointer" }}
           >
-              {key.charAt(0).toUpperCase() + key.slice(1)} {getSortIcon(key)}
-          </Box>
+            {key.charAt(0).toUpperCase() + key.slice(1)} {getSortIcon(key)}
+        </Box>
         </Styledtableheadercell>
       ))}
     </TableRow>
