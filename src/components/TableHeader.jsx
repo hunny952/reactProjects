a63d825ec1b335087ac8e84ad6704585904271fc
hunny952 <engineerhunny@gmail.com>
2024-07-02 16:58:25 +0500

    add name field into map loop

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,91 +1,12 @@
-// import React from "react";
-// import { TableCell, TableRow, Box } from "@mui/material";
-// import styled from "@emotion/styled";
-// import "../Components.css";
-
-// const Styledtableheadercell = styled(TableCell)({
-//       fontFamily: "system-ui",
-//       fontSize: "0.998rem",
-//       lineHeight: "2.6rem",
-//       borderBottom: "5px solid rgba(224, 224, 224, 1)",
-//       padding: "16px",
-//       color: "rgb(166 165 165)",
-//     })
-
-// const TableHeader = ({ handleSort, getSortIcon }) => {
-//   const headerCells = [
-//     { key: "hair_color", label: "Hair Color" },
-//     { key: "skin_color", label: "Skin Color" },
-//     { key: "gender", label: "Gender" },
-//     { key: "eye_color", label: "Eye Color" },
-//     { key: "birth_year", label: "Birth Year" },
-//   ];
-
-//   const getHeaderCellClassName = (key) => {
-//     switch (key) {
-//       case "Hair Color":
-//         return "header-cell-hair-color";
-//       case "Skin Color":
-//         return "header-cell-skin-color";
-//       case "Eye Color":
-//         return "header-cell-eye-color";
-//       case "Birth Year":
-//         return "header-cell-birth-year";
-//       case "Gender":
-//         return "header-cell-gender";
-//       default:
-//         return "header-cell-default";
-//     }
-//   };
-
-//   return (
-//     <TableRow>
-//       <Styledtableheadercell>
-//         <Box className="header-cell-box">
-//           <input
-//             className="header-cell-checkbox"
-//             type="checkbox"
-//           />
-//           <Box className="header-cell-column">
-//             <Box
-//               onClick={() => handleSort("name")}
-//               className="name-header-styling"
-//               style={{ cursor: "pointer" }}  
-//             >
-//               Name {getSortIcon("name")}
-//             </Box>
-//           </Box>
-//         </Box>
-//       </Styledtableheadercell>
-
-//       {headerCells.map(({ key, label }) => (
-//         <Styledtableheadercell key={key}>
-//         <Box
-//           onClick={() => handleSort(key)}
-//           className={`${getHeaderCellClassName(key)}`}
-//           style={{ cursor: "pointer" }}
-//           >
-//             {label} {getSortIcon(key)}
-//         </Box>
-//         </Styledtableheadercell>
-//       ))}
-//     </TableRow>
-//   );
-// };
-
-// export default TableHeader;
-
-
-
 import React from "react";
 import { TableCell, TableRow, Box, TextField } from "@mui/material";
 import styled from "@emotion/styled";
 import "../Components.css";
 
-const Styledtableheadercell = styled(TableCell)({
+const StyledTableHeaderCell = styled(TableCell)({
   fontFamily: "system-ui",
   fontSize: "0.998rem",
-  lineHeight: "2.6rem",
+  lineHeight: "1.6rem",
   borderBottom: "5px solid rgba(224, 224, 224, 1)",
   padding: "16px",
   color: "rgb(166 165 165)",
@@ -93,6 +14,7 @@ const Styledtableheadercell = styled(TableCell)({
 
 const TableHeader = ({ handleSort, getSortIcon }) => {
   const headerCells = [
+    { key: "name", label: "Name" },
     { key: "hair_color", label: "Hair Color" },
     { key: "skin_color", label: "Skin Color" },
     { key: "gender", label: "Gender" },
@@ -102,23 +24,13 @@ const TableHeader = ({ handleSort, getSortIcon }) => {
 
   return (
     <TableRow>
-      <Styledtableheadercell>
-        <Box className="header-cell-box">
-          <TextField className="header-cell-checkbox" type="checkbox" />
-          <Box className="header-cell-column">
-            <Box
-              onClick={() => handleSort("name")}
-              className="name-header-styling"
-              style={{ cursor: "pointer" }}
-            >
-              Name {getSortIcon("name")}
-            </Box>
-          </Box>
-        </Box>
-      </Styledtableheadercell>
-
       {headerCells.map(({ key, label }) => (
-        <Styledtableheadercell key={key}>
+        <StyledTableHeaderCell key={key}>
+          {key === "name" && (
+            <Box className="header-cell-box">
+              <TextField className="header-cell-checkbox" type="checkbox" />
+            </Box>
+          )}
           <Box
             onClick={() => handleSort(key)}
             className={`header-cell-${label.toLowerCase().replace(/\s+/g, "-")}`}
@@ -126,7 +38,7 @@ const TableHeader = ({ handleSort, getSortIcon }) => {
           >
             {label} {getSortIcon(key)}
           </Box>
-        </Styledtableheadercell>
+        </StyledTableHeaderCell>
       ))}
     </TableRow>
   );
